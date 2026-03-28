14c73a1003ccb05ff97f4a162a65cb6c1f21a73f
Ali Mohamed <Ali131999@GitHub.com>
2026-03-28 21:06:34 +0200

    initialized app

---
+++ lib/core/utils/app_theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    scaffoldBackgroundColor: AppColors.white,
+    appBarTheme: AppBarTheme(
+      backgroundColor: AppColors.white,
+      centerTitle: true,
+      elevation: 0,
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: AppColors.white,
+      type: BottomNavigationBarType.fixed,
+      elevation: 0,
+      selectedIconTheme: IconThemeData(color: AppColors.white),
+      unselectedIconTheme: IconThemeData(color: AppColors.blue),
+    ),
+  );
+}
